d18aab34b9966ad70cfaabde92eff201f588eb6e
Yuna Ukawa <91641645+ukaway@users.noreply.github.com>
2022-04-19 01:14:45 -0400

    Add files via upload

---
+++ domino.py
@@ -0,0 +1,21 @@
+from TableModule import Table
+
+print("== Calculate the number of tilings of a grid (4 x W) with dominoes (2 x 1) ==")
+
+print("\nEnter the number (1 ≤ n ≤ 1000) of datasets that follow:")
+while True:
+    n = int(input())
+    if 1 <= n <= 1000:
+        break
+    print("Try again:")
+
+result = []
+print("\nEnter values for W ():")
+for i in range(0, n):
+    t = Table()
+    t.placeDomino()
+    result.append(t.count)
+
+print("\nResults:")
+for i in range(0, n):
+    print(i + 1, result[i])
